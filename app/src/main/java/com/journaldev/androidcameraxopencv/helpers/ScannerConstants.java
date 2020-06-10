cde27d53fba7e170c78894c262cdb6f27129e51d
Vu Hoang Giang <giang@murakumo-tech.jp>
2020-06-11 00:16:25 +0700

    edit tect

--- app/src/main/java/com/journaldev/androidcameraxopencv/helpers/ScannerConstants.java
+++ app/src/main/java/com/journaldev/androidcameraxopencv/helpers/ScannerConstants.java
@@ -9,7 +9,7 @@ public class ScannerConstants {
     public static boolean analyzing = true;
     public static MatOfPoint2f croptedPolygon;
     public static Bitmap selectedImageBitmap;
-    public static String cropText="Crop",backText="Close",
+    public static String cropText="Crop",backText="Retake",
             imageError="No images selected, please try again.",
             cropError="You have not selected a valid field. Please correct it until the lines turn blue.";
     public static String cropColor="#6666ff",backColor="#ff0000",progressColor="#331199";
