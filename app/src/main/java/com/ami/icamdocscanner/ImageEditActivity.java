38fa030aef967b35890cd29f7cdc0b17627a2840
Vu Hoang Giang <giang@murakumo-tech.jp>
2020-06-28 19:25:21 +0700

    save to pdf

--- app/src/main/java/com/ami/icamdocscanner/ImageEditActivity.java
+++ app/src/main/java/com/ami/icamdocscanner/ImageEditActivity.java
@@ -2,13 +2,31 @@ package com.ami.icamdocscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.widget.ImageView;
 
 import com.ami.icamdocscanner.R;
 import com.ami.icamdocscanner.helpers.ScannerConstants;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Image;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Rectangle;
+import com.lowagie.text.pdf.PdfName;
+import com.lowagie.text.pdf.PdfString;
+import com.lowagie.text.pdf.PdfWriter;
 
 public class ImageEditActivity extends AppCompatActivity {
     private ScaleGestureDetector scaleGestureDetector;
@@ -23,6 +41,37 @@ public class ImageEditActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView);
         imageView.setImageBitmap(ScannerConstants.cropImageBitmap);
         scaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        ScannerConstants.cropImageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        byte[] byteArr = stream.toByteArray();
+
+        // step 1: creation of a document-object
+        Document document = new Document();
+        try {
+            // step 2:
+            // we create a writer that listens to the document
+            // and directs a PDF-stream to a file
+            PdfWriter instance = PdfWriter.getInstance(document, new FileOutputStream(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + "ami.pdf"));
+
+            Rectangle one = new Rectangle(ScannerConstants.cropImageBitmap.getWidth(),ScannerConstants.cropImageBitmap.getHeight());
+            document.setPageSize(one);
+            document.setMargins(0, 0, 0, 0);
+
+            // step 3: we open the document
+            document.open();
+            instance.getInfo().put(PdfName.CREATOR, new PdfString(Document.getVersion()));
+            // step 4: we add a paragraph to the document
+            Image img = Image.getInstance(byteArr);
+            document.add(img);
+
+        } catch (DocumentException | IOException de) {
+            System.err.println(de.getMessage());
+        }
+
+        // step 5: we close the document
+        document.close();
+
     }
 
     public boolean onTouchEvent(MotionEvent motionEvent) {
