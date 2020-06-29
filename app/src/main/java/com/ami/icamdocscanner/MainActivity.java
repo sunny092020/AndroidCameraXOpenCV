969aeb5b6b65ed6fa47ea880c6dae5a6f9e19663
Vu Hoang Giang <giang@murakumo-tech.jp>
2020-06-29 18:09:59 +0700

    add TODO

--- app/src/main/java/com/ami/icamdocscanner/MainActivity.java
+++ app/src/main/java/com/ami/icamdocscanner/MainActivity.java
@@ -328,6 +328,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         image.close();
     }
 
+    // TODO: multiple threads call this function concurrently
     private void takePicture() {
         File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "CAPTURE.jpg");
         ImageCapture.OutputFileOptions.Builder outputFileOptionsBuilder =
