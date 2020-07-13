b2fb2d9227155bc088ea125ea66c6963ef1ae784
Vu Hoang Giang <giang@murakumo-tech.jp>
2020-07-13 13:11:33 +0700

    use android built-in pdf library

--- app/src/main/java/com/ami/icamdocscanner/helpers/PdfUtils.java
+++ app/src/main/java/com/ami/icamdocscanner/helpers/PdfUtils.java
@@ -2,125 +2,83 @@ package com.ami.icamdocscanner.helpers;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.util.Log;
+import android.graphics.Canvas;
+import android.graphics.pdf.PdfDocument;
 
 import com.ami.icamdocscanner.models.RecyclerImageFile;
-import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
-import com.lowagie.text.Image;
-import com.lowagie.text.Rectangle;
-import com.lowagie.text.pdf.PdfName;
-import com.lowagie.text.pdf.PdfString;
-import com.lowagie.text.pdf.PdfWriter;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
 public class PdfUtils {
     public static void toPDFMulti(List<RecyclerImageFile> imageFiles, String outPath) {
-        toPDFMultiOpenPdf(imageFiles, outPath);
+        toPDFMultiAndroidBuiltIn(imageFiles, outPath);
     }
 
     public static void toPDFSingle(RecyclerImageFile imgFile, String outPath) {
-        toPDFSingleOpenPdf(imgFile, outPath);
+        toPDFSingleAndroidBuiltIn(imgFile, outPath);
     }
 
-    private static void toPDFMultiOpenPdf(List<RecyclerImageFile> imageFiles, String outPath) {
-        // step 1: creation of a document-object
-        Document document = new Document();
+    private static void toPDFMultiAndroidBuiltIn(List<RecyclerImageFile> imageFiles, String outPath) {
+        PdfDocument document = new PdfDocument();
 
-        // step 2:
-        // we create a writer that listens to the document
-        // and directs a PDF-stream to a file
-        PdfWriter instance;
-        try {
-            instance = PdfWriter.getInstance(document, new FileOutputStream(outPath + ".pdf"));
-            instance.getInfo().put(PdfName.CREATOR, new PdfString(Document.getVersion()));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        for(RecyclerImageFile imgFile: imageFiles) {
+            Bitmap imgBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+            PdfDocument.PageInfo pageInfo =new
+                    PdfDocument.PageInfo.Builder(imgBitmap.getWidth(), imgBitmap.getHeight(), 1).create();
+            PdfDocument.Page  page = document.startPage(pageInfo);
+
+            Canvas canvas = page.getCanvas();
+            canvas.drawBitmap(imgBitmap, 0f, 0f, null);
+            document.finishPage(page);
         }
 
-        for (int i = 0; i < imageFiles.size(); i++) {
-            RecyclerImageFile imgFile = imageFiles.get(i);
-
-            Bitmap imgBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-
-
-            imgBitmap.compress(Bitmap.CompressFormat.JPEG, 99, stream);
-            byte[] byteArr = stream.toByteArray();
-
-            try {
-                Rectangle one = new Rectangle(imgBitmap.getWidth(), imgBitmap.getHeight());
-                document.setPageSize(one);
-                document.setMargins(0, 0, 0, 0);
-
-                document.open();
-
-                if (i > 0) document.newPage();
-
-                Image img = Image.getInstance(byteArr);
-                document.add(img);
-
-            } catch (DocumentException | IOException de) {
-                System.err.println(de.getMessage());
-            } finally {
+        FileOutputStream os = null;
+        try {
+            os = new FileOutputStream(outPath + ".pdf");
+            document.writeTo(os);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (os!=null) {
                 try {
-                    stream.close();
+                    os.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        document.close();
+    }
+
+    private static void toPDFSingleAndroidBuiltIn(RecyclerImageFile imgFile, String outPath) {
+        PdfDocument document = new PdfDocument();
+        Bitmap imgBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+
+        PdfDocument.PageInfo pageInfo =new
+                PdfDocument.PageInfo.Builder(imgBitmap.getWidth(), imgBitmap.getHeight(), 1).create();
+        PdfDocument.Page  page = document.startPage(pageInfo);
+
+        Canvas canvas = page.getCanvas();
+        canvas.drawBitmap(imgBitmap, 0f, 0f, null);
+        document.finishPage(page);
+
+        FileOutputStream os = null;
+        try {
+            os = new FileOutputStream(outPath + ".pdf");
+            document.writeTo(os);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (os!=null) {
+                try {
+                    os.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
-
-        document.close();
-    }
-
-    private static void toPDFSingleOpenPdf(RecyclerImageFile imgFile, String outPath) {
-        // step 1: creation of a document-object
-        Document document = new Document();
-
-        // step 2:
-        // we create a writer that listens to the document
-        // and directs a PDF-stream to a file
-        PdfWriter instance;
-        try {
-            instance = PdfWriter.getInstance(document, new FileOutputStream(outPath + ".pdf"));
-            instance.getInfo().put(PdfName.CREATOR, new PdfString(Document.getVersion()));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        Bitmap imgBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-
-
-        imgBitmap.compress(Bitmap.CompressFormat.JPEG, 99, stream);
-        byte[] byteArr = stream.toByteArray();
-
-        try {
-            Rectangle one = new Rectangle(imgBitmap.getWidth(), imgBitmap.getHeight());
-            document.setPageSize(one);
-            document.setMargins(0, 0, 0, 0);
-            document.open();
-            Image img = Image.getInstance(byteArr);
-            document.add(img);
-
-        } catch (DocumentException | IOException de) {
-            System.err.println(de.getMessage());
-        } finally {
-            try {
-                stream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
         document.close();
     }
 }
