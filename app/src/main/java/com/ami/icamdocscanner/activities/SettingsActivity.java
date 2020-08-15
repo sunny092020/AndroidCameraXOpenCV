0cdb06e5eb26135dc59cde3e71d550cd8c73aa6b
Vu Hoang Giang <itworkjp@gmail.com>
2020-08-15 14:57:34 +0700

    fix preferences

--- app/src/main/java/com/ami/icamdocscanner/activities/SettingsActivity.java
+++ app/src/main/java/com/ami/icamdocscanner/activities/SettingsActivity.java
@@ -1,11 +1,15 @@
 package com.ami.icamdocscanner.activities;
 
 import android.os.Bundle;
+import android.util.Log;
+
 import com.ami.icamdocscanner.R;
+import com.ami.icamdocscanner.helpers.Preferences;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.preference.CheckBoxPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
@@ -32,14 +36,17 @@ public class SettingsActivity extends AppCompatActivity implements PreferenceFra
     }
 
     @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
-    }
+    public void onPointerCaptureChanged(boolean hasCapture) {}
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.layout.preferences, rootKey);
+            CheckBoxPreference autoCrop = findPreference(getString(R.string.is_crop_after_each_capture_key));
+            autoCrop.setOnPreferenceChangeListener((preference, newValue) -> {
+                Preferences.setIsCropAfterEachCapture(this.getActivity(), (Boolean) newValue);
+                return true;
+            });
         }
     }
 }
