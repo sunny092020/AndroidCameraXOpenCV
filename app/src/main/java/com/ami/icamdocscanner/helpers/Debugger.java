daf2f25d6025cc9016001d7dd27134db92c9ce93
Vu Hoang Giang <itworkjp@gmail.com>
2020-08-17 16:57:07 +0700

    add vision debugger

--- app/src/main/java/com/ami/icamdocscanner/helpers/Debugger.java
+++ app/src/main/java/com/ami/icamdocscanner/helpers/Debugger.java
@@ -1,4 +1,10 @@
 package com.ami.icamdocscanner.helpers;
 
+import android.graphics.Bitmap;
+
+import org.opencv.core.Mat;
+
 public class Debugger {
+    public static Mat mat;
+    public static Bitmap bitmap;
 }
