c529c25eaaed71627fe16f6261201e6019904561
Vu Hoang Giang <itworkjp@gmail.com>
2020-08-18 01:43:01 +0700

    add opencv c++

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,9 @@
 #include <jni.h>
 #include <string>
+#include <vector>
+#include <opencv2/core.hpp>
+
+#include <android/log.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_ami_icamdocscanner_activities_ImageDoneActivity_stringFromJNI(
@@ -9,3 +13,10 @@ Java_com_ami_icamdocscanner_activities_ImageDoneActivity_stringFromJNI(
     return env->NewStringUTF(hello.c_str());
 }
 
+extern "C" JNIEXPORT void JNICALL
+Java_com_ami_icamdocscanner_helpers_VisionUtils_houghLines(
+        JNIEnv* env,
+        jobject /* this */,
+        jlong mat) {
+
+}
